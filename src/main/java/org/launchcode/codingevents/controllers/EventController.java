3e4d3f2900d89bf40cf9078be8ceef554ca12e31
denysV1978 <denysvuk@gmail.com>
2019-12-19 17:52:00 -0600

    Model binding is done

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -1,12 +1,10 @@
 package org.launchcode.codingevents.controllers;
 
+import org.launchcode.codingevents.data.EventData;
 import org.launchcode.codingevents.models.Event;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -18,12 +16,13 @@ import java.util.List;
 @RequestMapping("events")
 public class EventController {
 
-    private static List<Event> events = new ArrayList<>();
+    //private static List<Event> events = new ArrayList<>(); now we remove this after we created EventData because now we gonna store all events in EventData
+
 
     @GetMapping()
     public String displayAllEvents(Model model) {
 
-        model.addAttribute("events", events);
+        model.addAttribute("events", EventData.getAll()); //now we change how we get values using EventData!!!!! the thing is that this is a class that will never be instantiated
         return "events/index";
     }
 
@@ -36,14 +35,37 @@ public class EventController {
 
     //lives at /events/create
     @PostMapping("create")
-    public String createEvent(@RequestParam String eventName, @RequestParam String eventDescription) {
-        events.add(new Event(eventName, eventDescription)); // this basically means that we push object in the List instead of just Strings. So,
+    public String createEvent(@ModelAttribute Event newEvent) { //so, this idea is about that @ModelAttribute will find necessary parameters for
+        //creating new Event newEvent and create this newEvent and after push it in EventData list. Do not forget that variables in html in this case should have the same names as variables in Event class
+        EventData.add(newEvent); // this basically means that we push object in the List instead of just Strings. So,
         //every time we have new event thrown from TH we create new object Event and push it in List events!!!
         return "redirect:"; //this means it sends to a method that handles this level of url. In our case it is /events (this is when there is nothing after : . If I
         //wanted to return it to the method handling (controlling) url events/create, I would type redirect:create. And it would run again method
         //createEvent...
 
 
+    }
+
+    @GetMapping("delete") //this guy will send info to th: html. Will be triggered by url .../delete
+    public String displayDeleteEventForm(Model model) {
+
+        model.addAttribute("title", "Delete Events");
+        model.addAttribute("events", EventData.getAll());
+        return "events/delete";
+    }
+
+    @PostMapping("delete") // this is the way to call this controller is when user hits Submit button
+    public String processDeleteEventsForm(@RequestParam(required = false) int[] eventIds) { // looks like this required means that it is not required to have eventIds during running of this controller
+
+       // System.out.println("Stop");
+
+        if (eventIds != null) {
+            for (int id : eventIds) {
+                EventData.remove(id);
+            }
+        }
+
+        return "redirect:";
     }
 
 }
